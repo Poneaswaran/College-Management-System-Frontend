e1b9e6805b7ea1324d8bb4c683efdfd8e3b80131
Poneaswaran <easwar86@yahoo.com>
2026-02-12 18:02:16 +0530

    fix: Correct base path in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/College-Management-System-Frontend/',
   server: {
     host: true,
     port: 3000,
